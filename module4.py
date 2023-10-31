1ecdfe406ba7b304a5783b65fc05d408ef114dc5
Clifforddd <90348904+Clifforddd@users.noreply.github.com>
2023-10-30 21:12:47 -0400

    Update module4.py

--- module4.py
+++ module4.py
@@ -1,11 +1,17 @@
-print("Enter your a integer:")
-y = input()
-print("" + y)
+N = int(input("Enter a positive integer N: "))
 
-print("Enter another X for integer")
-x = input()
+numbers = []
 
-for i in x:
-  print(i)
+for i in range(N):
+    number = int(input(f"Enter number {i + 1}: "))
+    numbers.append(number)
+
+X = int(input("Enter an integer X: "))
+
+if X in numbers:
+    index = numbers.index(X) + 1  # Adding 1 to make it 1-based index
+    print(f"The index of {X} is: {index}")
+else:
+    print("-1")
 
 
